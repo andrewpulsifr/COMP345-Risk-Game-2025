94553d9055e4632344ebb2fe3cfdc848c6eec6bc
Andrew Pulsifer <z5673825@ad.unsw.edu.au>
2025-10-07 13:32:49 -0400

    feat(addDriver): driver implementation

--- drivers/MainDriver.cpp
+++ drivers/MainDriver.cpp
@@ -3,19 +3,23 @@
 #include "../include/Player.h"
 #include "../include/Orders.h"
 #include "../include/Cards.h"
+#include "../include/GameEngine.h"
 
 // Forward declarations of driver test functions
-// void testLoadMaps();
+void testLoadMaps();
 void testPlayers();
 void testOrdersLists();
-// void testCards();
+void testCards();
+void testGameStates();
 
 int main() {
     std::cout << "=== Starting Warzone Test Drivers ===\n\n";
-    // testLoadMaps();
+    testLoadMaps();
     testPlayers();
     testOrdersLists();
-    // testCards();
+    testCards();
+    testGameStates();
+    std::cout << "\n";
     std::cout << "\n=== Program finished successfully ===\n";
     return 0;
 }
