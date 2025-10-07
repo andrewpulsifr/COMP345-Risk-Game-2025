0643143eca97aba2487fb3c0890227e82aa755cc
Roman Stikhin <romanstikhin@romans-air.home>
2025-10-07 13:34:45 -0400

    Sync MainDriver.cpp and MapDriver.cpp from Desktop copy

--- drivers/MainDriver.cpp
+++ drivers/MainDriver.cpp
@@ -3,12 +3,14 @@
 #include "../include/Player.h"
 #include "../include/Orders.h"
 #include "../include/Cards.h"
+#include "../include/GameEngine.h"
 
 // Forward declarations of driver test functions
 void testLoadMaps();
 void testPlayers();
 void testOrdersLists();
 void testCards();
+void testGameStates();
 
 int main() {
     std::cout << "=== Starting Warzone Test Drivers ===\n\n";
@@ -17,6 +19,7 @@ int main() {
     testPlayers();
     testOrdersLists();
     testCards();
+    testGameStates();
     std::cout << "\n=== Program finished successfully ===\n";
     return 0;
 }
