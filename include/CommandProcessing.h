135b5ce7ea285bdf021e4dedfc57894b4095dc2b
Chhayhee Sok <soooooook777@gmail.com>
2025-10-16 23:34:39 -0400

    Implemented some of CommandProcessing methods in cpp file

--- include/CommandProcessing.h
+++ include/CommandProcessing.h
@@ -6,20 +6,30 @@ class Command;
 
 class CommandProcessor {
     public:
-        void saveCommand();
+        CommandProcessor(); // Default Constructor.
+        CommandProcessor(const CommandProcessor &obj); // Deep copy constructor for CommandProcessor.
         Command* getCommand();
+        std::vector<Command*> getCommandObjects;
         void saveEffect();
+        boolean validate();
 
     private:
         void readCommand(std::string command);
+        void saveCommand();
         std::vector<Command*> commandObjects;
 
 }
 
 class FileCommandProcessorAdapter {
     public:
+        void saveCommand();
+        Command* getCommand();
+        void saveEffect();
+        boolean validate();
 
     private:
         void readCommand(std::string command);
-        
+        std::vector<Command*> commandObjects;
+
+
 }
--- src/CommandProcessing.cpp
+++ src/CommandProcessing.cpp
@@ -1,6 +1,23 @@
 #include "../include/GameEngine.h"
 
+#include <vector>
 
-void testCommandProcessor {
-    
+// Default constructor for CommandProcessor.
+CommandProcessor::CommandProcessor() : commandObjects() {}
+
+// Constructor that takes
+
+// Deep copy constructor for CommmandProcessor.
+CommandProcessor::CommandProcessor(CommandProcessor &obj) {
+    for(int i = 0; i < obj.getCommandObjects.size(); i++) {
+        Command* commandptr = obj.getCommandObjects[i];
+
+        if(commamndptr) {
+            Command* newCopy = new Command(obj.getName());
+            this->commandObjects.push_back(newCopy);
+        }
+    }
 }
+
+// 
+
