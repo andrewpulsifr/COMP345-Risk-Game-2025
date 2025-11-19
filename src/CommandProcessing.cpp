77d487220515c5b98da0364a05a3d416de0c3743
Chhayhee Sok <soooooook777@gmail.com>
2025-11-19 17:55:07 -0500

    Implements 2 new methods in CommandProcessor: validateTournament and cleanWhiteSpace.

--- src/CommandProcessing.cpp
+++ src/CommandProcessing.cpp
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include <sstream> // To import istringstream to continuously from a string.
 
 
 
@@ -52,7 +53,7 @@ CommandProcessor::~CommandProcessor() {
 
     commandObjects.clear();
     
-    std::cout << "** The CommandProcessor object is destroyed." << std::endl;
+    // std::cout << "** The CommandProcessor object is destroyed." << std::endl;
 }
 
 /**
@@ -178,6 +179,137 @@ std::string CommandProcessor::stringToLog() const {
     return "CommandProcessor: Saved command - " + lastCommand->getName();
 }
 
+
+// ===== A3, Part 2: Tournament Mode =====
+
+/** @brief A Helper Function, that clean the whitespace of commands to extract only the string entered.
+ * @param the command entered.
+ * @return the cleaned string.
+ */
+std::string CommandProcessor::cleanWhiteSpace(const std::string& command) {
+    size_t start = command.find_first_not_of(" \t\r\n");
+    size_t end = command.find_last_not_of(" \t\r\n");
+
+    std::string cleanedCommand = command.substr(start, end - start + 1);
+
+    return cleanedCommand;
+}
+
+/** 
+ * @brief To validate the parameters entered in the line of tournament command.
+ * @return A vector of int values, that corresponds to the value of -M, -P, -G, -D.
+ */
+std::vector<int> CommandProcessor::validateTournament(const std::string& command) {
+    std::cout << "  -> Validating Tournament...\n" << std::endl;
+    
+    // Temporary vector to store and return the validate values.
+    std::vector<int> tournamentValues;
+
+    // Temp variables and vector for extracting and storing map and player names in the while loop below.
+    std::string nameExtracted; // temp string used in while loop below to store each map/player name in the vector.
+    std::vector<std::string> mapNames;
+    std::vector<std::string> playerNames;
+
+
+    // Get the position index of each parameter, used to extract their respective values.
+    std::size_t listOfMapsIndex = command.find("-M");
+    std::size_t listOfPlayersIndex = command.find("-P");
+    std::size_t numOfGamesIndex = command.find("-G");
+    std::size_t maxNumOfTurnsIndex = command.find("-D");
+
+    // Throw an exception if the parameters or their values are not found.
+    if(listOfMapsIndex == std::string::npos || listOfPlayersIndex == std::string::npos || numOfGamesIndex == std::string::npos || maxNumOfTurnsIndex == std::string::npos) {
+        throw std::invalid_argument("");
+    }
+
+
+
+
+    // Extract the list of Maps, entered after -M and before -P, excluding the letters themselves.
+    std::string listOfMapsStr = command.substr(listOfMapsIndex + 2, listOfPlayersIndex - listOfMapsIndex - 2);
+    listOfMapsStr = cleanWhiteSpace(listOfMapsStr);
+
+    // Process the Maps by validating and counting the number of maps entered.
+    
+        // Use istringstream to continuously read the list of maps entered and store it in a temp vector (called mapNames);
+        std::istringstream iss(listOfMapsStr);
+
+        while(iss >> nameExtracted) {
+            mapNames.push_back(nameExtracted);
+        }
+
+        // reset istringstream to be reused in the player validation below.
+        iss.clear();
+
+    std::size_t numOfMaps = mapNames.size(); // get size of the vector of map names.
+
+    // Check if it is a valid value. If the value is invalid, an exception is thrown and should be caught in the main command loop that asks user to re-enter a command.
+    if(numOfMaps < 1 || numOfMaps > 5)
+        throw std::out_of_range("The number of Map(s) entered is invalid. Please re-enter the tournament command, with a -M value between 1 - 5.");
+
+    std::cout << "    The Number of Maps is valid, with a value of: " << numOfMaps << std::endl;
+    tournamentValues.push_back(numOfMaps);
+
+    
+    
+
+    // Extract the number of Players.
+    std::string listOfPlayersStr = command.substr(listOfPlayersIndex + 2, numOfGamesIndex - listOfPlayersIndex - 2);
+    listOfPlayersStr = cleanWhiteSpace(listOfPlayersStr);
+
+    // Process the Maps by validating and counting the number of maps entered.
+    
+        // Reuse istringstream declared in map above to read the list of players entered and store it in a temp vector (called playerNames);
+        iss.str(listOfPlayersStr);
+
+        while(iss >> nameExtracted) {
+            playerNames.push_back(nameExtracted);
+        }
+
+    std::size_t numOfPlayers = playerNames.size(); // get size of the vector of player names.
+
+    if(numOfPlayers < 2 || numOfPlayers > 4)
+        throw std::out_of_range("The number of Player(s) entered is invalid. Please re-enter the tournament command, with a -P value between 2 - 4.");
+    
+    std::cout << "    The Number of Players is valid, with a value of: " << numOfPlayers << std::endl;
+    tournamentValues.push_back(numOfPlayers);
+
+
+
+
+
+    // Extract the number of Games.
+    std::string numOfGamesStr = command.substr(numOfGamesIndex + 2, maxNumOfTurnsIndex - numOfGamesIndex - 2);
+    numOfGamesStr = cleanWhiteSpace(numOfGamesStr);
+
+    int numOfGames = std::stoi(numOfGamesStr); // convert from string to int value.    
+
+    if(numOfGames < 1 || numOfGames > 5)
+            throw std::out_of_range("The number of Game(s) entered is invalid. Please re-enter the tournament command, with a -G value between 1 and 5.");
+
+    std::cout << "    The Number of Games is valid, with a value of: " << numOfGames << std::endl;
+    tournamentValues.push_back(numOfGames);
+
+
+    // Extract the max number of Turns.
+    std::string maxNumOfTurnsStr = command.substr(maxNumOfTurnsIndex + 2);
+    maxNumOfTurnsStr = cleanWhiteSpace(maxNumOfTurnsStr);
+
+    int maxNumOfTurns = std::stoi(maxNumOfTurnsStr); // convert from string to int value.
+
+    if(maxNumOfTurns < 10 || maxNumOfTurns > 50)
+        throw std::out_of_range("The number of maximum turn(s) entered is invalid. Please re-enter the tournament command, with a -D value between 10 and 50.");
+
+    std::cout << "    The Max Number of Turns is valid, with a value of: " << maxNumOfTurns << std::endl;
+    tournamentValues.push_back(maxNumOfTurns);
+
+        
+    std::cout << "\n    The Tournament Command is valid!\n" << std::endl;
+
+    return tournamentValues;
+}
+
+
 /**
  * @brief getCommand(): for GameEngine or Player objects to read and process commands from the command line.
  * @param engine, passed to process command and to display game status.
@@ -216,7 +348,24 @@ void CommandProcessor::getCommand(GameEngine& engine) {
         // Save the full lineEntered(command + mapname/playername if present) and get its pointer.
         Command* cmdptr = saveCommand(lineEntered);
 
+        // Check to see if the comand is valid in the current state.
         bool validCommand = validate(engine, *cmdptr);
+
+        // A3, P2 - ADDITIONAL LAYER OF CHECKING FOR TOURNAMENT COMMAND:
+        // See if the command is a valid tournament command line. If a value of the parameter (-M, -P, -G, -D) is invalid, an exception will be thrown.
+        // And the bool validCommand will be set to false, error message is printed, and command will not be processed.
+        if(commandEntered == "tournament" && validCommand) {
+            try {
+                validateTournament(lineEntered);
+            } catch(const std::out_of_range& valueErr) { // Catch out-of-range errors in the parameters of 'tournament'.
+                std::cout << "ERROR 1: " << valueErr.what() << std::endl;
+                validCommand = false;
+            } catch(const std::invalid_argument& invalidErr) { // Catch invalid 'tournament' without enough parameters/ whitespace.
+                std::cout << "ERROR 2: Please enter a valid 'tournament' command in the following format:\n" <<
+                             "       tournament -M <listofmapfiles> -P <listofplayerstrategies> -G <numberofgames> -D <maxnumberofturns>" << std::endl;
+                validCommand = false;
+            }
+        }
 
         // If Command is valid, process the command to trigger state transition.
         if(validCommand) {
@@ -263,7 +412,7 @@ FileCommandProcessorAdapter::~FileCommandProcessorAdapter() {
         file.close();
     }
 
-    std::cout << "** The FileCommandProcessorAdapter object is destroyed." << std::endl;
+    // std::cout << "** The FileCommandProcessorAdapter object is destroyed." << std::endl;
 }
 
 /**
